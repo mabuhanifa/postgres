b99f83d5bf489eb73af5665896a6cd85c05df58d
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-13 04:07:03 +0600

    created book

--- server.js
+++ server.js
@@ -7,25 +7,48 @@ app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 5000;
 
-const user ={
-  user: process.env.user,
-  password: process.env.password,
-  host: process.env.host,
-  port: 5432,
-  database: process.env.database,
-};
-
 // get all todos
-app.get("/todos", async (req, res) => {
+app.get("/book", async (req, res) => {
   try {
-    const allTodos = await pool.query("SELECT * FROM employee");
+    const allTodos = await pool.query("SELECT * FROM bookdb");
     res.json(allTodos.rows);
   } catch (error) {
     console.error(error);
   }
 });
 
+app.get("/book/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await pool.query("SELECT * FROM bookdb WHERE id=$1", [id]);
+    res.status(201).json({
+      message: `success`,
+      data: book.rows,
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({ error: error.message });
+  }
+});
+
+app.post("/book", async (req, res) => {
+  try {
+    const { name, price, info, img } = req.body;
+    const newBook = await pool.query(
+      "INSERT INTO bookdb (name, price, info, img) VALUES($1, $2, $3, $4) RETURNING *",
+      [name, price, info, img]
+    );
+
+    res.status(201).json({
+      message: `book was created successfully ${name}`,
+      data: newBook.rows,
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({ error: error.message });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`App listening on ${port}`, user);
+  console.log(`App listening on ${port}`);
 });
