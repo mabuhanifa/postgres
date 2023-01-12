7c5cd59494d5d226ab4b80c005f42be0d7ef5488
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-13 01:31:33 +0600

    listen

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`App listening on ${port}`);
+});
