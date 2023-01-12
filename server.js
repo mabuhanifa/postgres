ec5eb9b6acf6ab5fa221b2786d9289c86ca08239
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-13 05:21:33 +0600

    script

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // get all todos
 app.get("/book", async (req, res) => {
@@ -17,6 +17,7 @@ app.get("/book", async (req, res) => {
   }
 });
 
+// get a book by id
 app.get("/book/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -31,6 +32,21 @@ app.get("/book/:id", async (req, res) => {
   }
 });
 
+// delete a book by id
+app.delete("/book/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await pool.query("DELETE FROM bookdb WHERE id=$1", [id]);
+    res.status(201).json({
+      message: `successfully deleted book ${id}`,
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({ error: error.message });
+  }
+});
+
+// create a book
 app.post("/book", async (req, res) => {
   try {
     const { name, price, info, img } = req.body;
@@ -42,6 +58,26 @@ app.post("/book", async (req, res) => {
     res.status(201).json({
       message: `book was created successfully ${name}`,
       data: newBook.rows,
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({ error: error.message });
+  }
+});
+
+// update a book by id
+app.put("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, price, info } = req.body;
+    const updatedBook = await pool.query(
+      "UPDATE book SET name=$1, price=$2 info=$3 WHERE id=$4 RETURNING *",
+      [name, price, info, id]
+    );
+
+    res.status(201).json({
+      message: `book was updated successfully ${id} ,${name}, ${info}`,
+      data: updatedBook.rows,
     });
   } catch (error) {
     console.error(error);
